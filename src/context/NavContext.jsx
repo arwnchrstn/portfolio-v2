bf4bc7272d59a2d8a4b89cc53c8179e2dbd8b5b8
arwnchrstn <ceres703@gmail.com>
2023-01-08 21:08:28 +0800

    fix

--- src/context/NavContext.jsx
+++ src/context/NavContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import { navReducer } from "../reducer/NavReducer";
+import { navReducer } from "../reducer/navReducer";
 
 export const NavContext = createContext({});
 
